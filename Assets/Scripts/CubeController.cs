b522e4e2ceadfd3afcd9aa3694fb5ae46858d3a1
douglevey1 <douglevey1@gmail.com>
2019-10-19 11:35:30 -0400

    Part 1

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    public void OnMouseDown()
+    {
+        //If this game object is not activeCube, it will turn white
+        if (GameController.activeCube != null)
+        {
+            GameController.activeCube.GetComponent<Renderer>().material.color = Color.white;
+        }
+        //Turns clicked cube red
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        GameController.activeCube = gameObject;
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public static GameObject activeCube;
+    public Vector3 cubePos;
+    public float xPos;
+    public float yPos;
+
+    public void Start()
+    {
+        xPos = -15;
+        yPos = 0;
+        //Spawns 16 cubes in a row
+        for (int x = 0; x < 16; x++)
+        {
+            cubePos = new Vector3(xPos, yPos, 0);
+            Instantiate(cubePrefab, cubePos, Quaternion.identity);
+            xPos += 2;
+        }
+    }
+
+    public void Update()
+    {
+        
+    }
+}
