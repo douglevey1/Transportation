9eb7ace4eabf35410131e61e0de167d1cd8f82ec
douglevey1 <douglevey1@gmail.com>
2019-10-24 12:48:05 -0400

    Part 2

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,23 +4,49 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    public Vector3 scaleUp;
+    public Vector3 scaleNorm;
+    public int upX = 1, upY = 1, upZ = 1;
+    public int normX = 1, normY = 1, normZ = 1;
     void Start()
     {
-        
+        //sets scaling
+        scaleUp = new Vector3(upX, upY, upZ);
+        scaleNorm = new Vector3(normX, normY, normZ);
     }
     public void OnMouseDown()
     {
-        //If this game object is not activeCube, it will turn white
-        if (GameController.activeCube != null)
+        //if the clicked cube is red, it will enlarge and open up to being moved through planeClicked
+        if (gameObject == GameController.activeCube && GameController.planeClicked == false)
         {
-            GameController.activeCube.GetComponent<Renderer>().material.color = Color.white;
+            GameController.planeClicked = true;
+            transform.localScale += scaleUp;
         }
-        //Turns clicked cube red
-        gameObject.GetComponent<Renderer>().material.color = Color.red;
-        GameController.activeCube = gameObject;
+        //if the red cube is already enlarged and is clicked on again, it will size back down and will not be moved if a different cube is clicked
+        else if(gameObject == GameController.activeCube && GameController.planeClicked == true)
+        {
+            GameController.planeClicked = false;
+            transform.localScale = scaleNorm;
+        }
+        //if the object clicked is not the red cube, but the red cube is enlarged/active, the clicked cube will become the new red cube
+        else if(gameObject != GameController.activeCube && GameController.planeClicked == true)
+        {
+            GameController.activeCube.transform.localScale = scaleNorm;
+            GameController.activeCube = gameObject;
+            GameController.planeClicked = false;
+        }
     }
     void Update()
     {
-        
+        //constantly checks if each cube is the active cube or not, and if it is, it becomes red
+        if(gameObject == GameController.activeCube)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            transform.localScale = scaleNorm;
+        }
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,19 +7,38 @@ public class GameController : MonoBehaviour
     public GameObject cubePrefab;
     public static GameObject activeCube;
     public Vector3 cubePos;
-    public float xPos;
-    public float yPos;
+    public float xPos, yPos;
+    public static GameObject[,] cubeGrid;
+    public static int gridX, gridY;
+    public static bool planeClicked = false;
+    int setX = 0, setY = 0;
 
     public void Start()
     {
+        //Grid size
+        gridX = 16;
+        gridY = 9;
+        cubeGrid = new GameObject[gridX, gridY];
+        //Physical cube positions
         xPos = -15;
-        yPos = 0;
-        //Spawns 16 cubes in a row
-        for (int x = 0; x < 16; x++)
+        yPos = 9;
+        //Spawns cubes and assigns them to each space in the cubeGrid array
+        for (int x = 0; x < (gridX * gridY); x++)
         {
             cubePos = new Vector3(xPos, yPos, 0);
-            Instantiate(cubePrefab, cubePos, Quaternion.identity);
+            cubeGrid[setX, setY] = Instantiate(cubePrefab, cubePos, Quaternion.identity);
             xPos += 2;
+            setX++;
+            //once cubes reach the end of the screen, the position is set back to the beginning
+            if(xPos > 15)
+            {
+                xPos = -15;
+                yPos -= 2;
+                setX = 0;
+                setY++;
+            }
+            //sets the first cube at position 0, 0 to be the active/red cube
+            activeCube = cubeGrid[0, 0];
         }
     }
 
